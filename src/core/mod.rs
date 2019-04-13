42450fef2747204e237a180b50cb82a496b05e43
Alexis Sellier <self@cloudhead.io>
2019-04-13 16:04:56 +0200

    Add TRANSPARENT color

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -26,6 +26,12 @@ impl Rgba {
         b: 1.0,
         a: 1.0,
     };
+    pub const TRANSPARENT: Self = Self {
+        r: 0.0,
+        g: 0.0,
+        b: 0.0,
+        a: 0.0,
+    };
 
     pub fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
         Self { r, g, b, a }
