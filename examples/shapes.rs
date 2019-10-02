9c09ce0069705b9792b1509eed977dae97fff76f
Diggory Hardy <git@dhardy.name>
2019-10-02 10:56:27 +0100

    Shapes example: move draw into request_redraw
    According to winit docs, this is preferable

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -10,7 +10,7 @@ use rgx::math::*;
 
 use raw_window_handle::HasRawWindowHandle;
 use winit::{
-    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event::{ElementState, Event, KeyboardInput, StartCause, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
@@ -42,9 +42,11 @@ fn main() {
 
     let mut textures = r.swap_chain(win.width as u32, win.height as u32, PresentMode::default());
     
-    let mut redraw = true;
-
     event_loop.run(move |event, _, control_flow| match event {
+        Event::NewEvents(StartCause::Init) => {
+            window.request_redraw();
+            *control_flow = ControlFlow::Wait;
+        }
         Event::WindowEvent { event, .. } => match event {
             WindowEvent::KeyboardInput {
                 input:
@@ -59,11 +61,11 @@ fn main() {
                     *control_flow = ControlFlow::Exit;
                 }
                 _ => {}
-            },
+            }
             WindowEvent::CursorMoved { position, .. } => {
                 mx = position.x;
                 my = position.y;
-                redraw = true;
+                window.request_redraw();
             }
             WindowEvent::CloseRequested => {
                 *control_flow = ControlFlow::Exit;
@@ -77,90 +79,80 @@ fn main() {
                 textures = r.swap_chain(w, h, PresentMode::default());
             }
             WindowEvent::RedrawRequested => {
-                redraw = true;
+                let rows = (win.height as f32 / sh) as u32;
+                let cols = (win.width as f32 / sw) as u32;
+
+                ///////////////////////////////////////////////////////////////////////////
+                // Prepare shape view
+                ///////////////////////////////////////////////////////////////////////////
+
+                let mut batch = Batch::new();
+                let (dx, dy) = ((mx / win.width) as f32, (my / win.height) as f32);
+
+                for i in 0..rows {
+                    let y = i as f32 * sh;
+
+                    for j in 0..cols {
+                        let x = j as f32 * sw - sw / 2.0;
+
+                        let c1 = i as f32 / rows as f32 + dy;
+                        let c2 = j as f32 / cols as f32 - dx;
+
+                        if j % 2 == 0 && i % 2 == 0 {
+                            batch.add(Shape::Circle(
+                                Point2::new(x + sw / 2., y + sw / 2.),
+                                sw * 2.,
+                                32,
+                                Stroke::new(1.0, Rgba::new(0.5, c2, c1, 0.75)),
+                                Fill::Empty(),
+                            ));
+                        }
+
+                        if j * i % 2 != 0 {
+                            batch.add(Shape::Rectangle(
+                                Rect::new(x, y, x + sw, y + sh),
+                                Stroke::new(3.0, Rgba::new(c1, c2, 0.5, 1.0)),
+                                Fill::Solid(Rgba::new(1.0, dx, dy, 0.1)),
+                            ));
+                        } else {
+                            batch.add(Shape::Line(
+                                Line::new(x, y, x + sw, y + sh),
+                                Stroke::new(
+                                    1.0,
+                                    Rgba::new(
+                                        i as f32 / rows as f32 + dy,
+                                        j as f32 / cols as f32 - dx,
+                                        0.5,
+                                        0.75,
+                                    ),
+                                ),
+                            ));
+                        };
+                    }
+                }
+
+                let buffer = batch.finish(&r);
+
+                ///////////////////////////////////////////////////////////////////////////
+                // Create frame
+                ///////////////////////////////////////////////////////////////////////////
+
+                let mut frame = r.frame();
+
+                ///////////////////////////////////////////////////////////////////////////
+                // Draw frame
+                ///////////////////////////////////////////////////////////////////////////
+
+                let out = textures.next();
+                {
+                    let pass = &mut frame.pass(PassOp::Clear(Rgba::TRANSPARENT), &out);
+
+                    pass.set_pipeline(&pip);
+                    pass.draw_buffer(&buffer);
+                }
+                r.submit(frame);
             }
             _ => {}
-        },
-        Event::EventsCleared => {
-            if !redraw {
-                // By only redrawing as necessary we reduce CPU usage
-                return;
-            }
-            *control_flow = ControlFlow::Wait;
-            redraw = false;
-
-            let rows = (win.height as f32 / sh) as u32;
-            let cols = (win.width as f32 / sw) as u32;
-
-            ///////////////////////////////////////////////////////////////////////////
-            // Prepare shape view
-            ///////////////////////////////////////////////////////////////////////////
-
-            let mut batch = Batch::new();
-            let (dx, dy) = ((mx / win.width) as f32, (my / win.height) as f32);
-
-            for i in 0..rows {
-                let y = i as f32 * sh;
-
-                for j in 0..cols {
-                    let x = j as f32 * sw - sw / 2.0;
-
-                    let c1 = i as f32 / rows as f32 + dy;
-                    let c2 = j as f32 / cols as f32 - dx;
-
-                    if j % 2 == 0 && i % 2 == 0 {
-                        batch.add(Shape::Circle(
-                            Point2::new(x + sw / 2., y + sw / 2.),
-                            sw * 2.,
-                            32,
-                            Stroke::new(1.0, Rgba::new(0.5, c2, c1, 0.75)),
-                            Fill::Empty(),
-                        ));
-                    }
-
-                    if j * i % 2 != 0 {
-                        batch.add(Shape::Rectangle(
-                            Rect::new(x, y, x + sw, y + sh),
-                            Stroke::new(3.0, Rgba::new(c1, c2, 0.5, 1.0)),
-                            Fill::Solid(Rgba::new(1.0, dx, dy, 0.1)),
-                        ));
-                    } else {
-                        batch.add(Shape::Line(
-                            Line::new(x, y, x + sw, y + sh),
-                            Stroke::new(
-                                1.0,
-                                Rgba::new(
-                                    i as f32 / rows as f32 + dy,
-                                    j as f32 / cols as f32 - dx,
-                                    0.5,
-                                    0.75,
-                                ),
-                            ),
-                        ));
-                    };
-                }
-            }
-
-            let buffer = batch.finish(&r);
-
-            ///////////////////////////////////////////////////////////////////////////
-            // Create frame
-            ///////////////////////////////////////////////////////////////////////////
-
-            let mut frame = r.frame();
-
-            ///////////////////////////////////////////////////////////////////////////
-            // Draw frame
-            ///////////////////////////////////////////////////////////////////////////
-
-            let out = textures.next();
-            {
-                let pass = &mut frame.pass(PassOp::Clear(Rgba::TRANSPARENT), &out);
-
-                pass.set_pipeline(&pip);
-                pass.draw_buffer(&buffer);
-            }
-            r.submit(frame);
         }
         _ => {}
     });
