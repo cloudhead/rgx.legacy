c3de863cb8e6e8d925be5418f85adb3d1009ae6b
Alexis Sellier <self@cloudhead.io>
2019-12-03 23:21:16 +0100

    Implement `TransformBuffer`
    This replaces the old `Model`, and implements a transform
    buffer for use with dynamic uniform buffers.

--- src/kit/mod.rs
+++ src/kit/mod.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 pub use crate::core;
 pub use crate::core::{Bgra8, Rgba, Rgba8};
 
@@ -193,8 +192,8 @@ pub fn ortho(w: u32, h: u32) -> Matrix4<f32> {
 #[derive(Copy, Clone)]
 pub struct AlignedBuffer {
     // TODO: Make this generic when rust-lang#43408 is fixed.
-    data: Matrix4<f32>,
-    padding: [u8; AlignedBuffer::PAD],
+    _data: Matrix4<f32>,
+    _padding: [u8; AlignedBuffer::PAD],
 }
 
 impl AlignedBuffer {
@@ -203,20 +202,22 @@ impl AlignedBuffer {
 
     pub fn new(data: Matrix4<f32>) -> Self {
         Self {
-            data,
-            padding: [0u8; AlignedBuffer::PAD],
+            _data: data,
+            _padding: [0u8; AlignedBuffer::PAD],
         }
     }
 }
 
-struct Model {
+pub struct TransformBuffer {
+    pub binding: core::BindingGroup,
+
     buf: core::UniformBuffer,
-    binding: core::BindingGroup,
     size: usize,
+    cap: usize,
 }
 
-impl Model {
-    fn new(
+impl TransformBuffer {
+    pub fn new(
         layout: &core::BindingGroupLayout,
         transforms: &[Matrix4<f32>],
         dev: &core::Device,
@@ -225,7 +226,51 @@ impl Model {
         let buf = dev.create_uniform_buffer(aligned.as_slice());
         let binding = dev.create_binding_group(&layout, &[&buf]);
         let size = transforms.len();
-        Self { buf, binding, size }
+
+        Self {
+            buf,
+            binding,
+            size,
+            cap: size,
+        }
+    }
+
+    pub fn with_capacity(
+        cap: usize,
+        layout: &core::BindingGroupLayout,
+        dev: &core::Device,
+    ) -> Self {
+        let aligned = AlignedBuffer::new(Matrix4::identity());
+
+        let mut transforms = Vec::with_capacity(cap);
+        transforms.resize(cap, aligned);
+
+        let buf = dev.create_uniform_buffer(transforms.as_slice());
+        let binding = dev.create_binding_group(&layout, &[&buf]);
+
+        Self {
+            buf,
+            binding,
+            size: 0,
+            cap,
+        }
+    }
+
+    pub fn offsets(&self) -> std::iter::StepBy<std::ops::Range<u64>> {
+        let max: u64 = self.size as u64 * AlignedBuffer::ALIGNMENT;
+        (0..max).step_by(AlignedBuffer::ALIGNMENT as usize)
+    }
+
+    pub fn update(&mut self, transforms: &[Matrix4<f32>], r: &core::Renderer, f: &mut core::Frame) {
+        let len = transforms.len();
+        assert!(len <= self.cap, "fatal: capacity exceeded");
+
+        let data = Self::aligned(transforms);
+        let src = r.uniform_buffer(data.as_slice());
+
+        f.copy(&src, &self.buf);
+
+        self.size = len;
     }
 
     fn aligned(transforms: &[Matrix4<f32>]) -> Vec<AlignedBuffer> {
